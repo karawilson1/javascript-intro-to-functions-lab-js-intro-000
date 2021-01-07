b4670b69800beb3eac4009417a8f953e14b5a28a
Wilson <karaewilson16@gmail.com>
2021-01-07 16:08:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!";
+  return "YES INDEED!";
+  var lowercase = "hello!";
+  return "I can't hear you!";
+  var mixedCase = "I love you, Grandma.";
+  return "I love you, too.";
+}

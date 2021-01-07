ca3eb7a0a733f04441d778b42fb729836dc5db77
Wilson <karaewilson16@gmail.com>
2021-01-07 17:09:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if ('string') === string.toUpperCase()
+  if string === string.toUpperCase()
     return "YES INDEED!";
-  if ('string') === string.toLowerCase()
+  if string === string.toLowerCase()
     return "I can't hear you!";
-  if ('string') === string.toUpperCase() && ('string') === string.toLowerCase()
+  if string === string.toUpperCase() && string === string.toLowerCase()
     return "I love you, too.";
 }
